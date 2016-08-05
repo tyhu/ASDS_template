188ba2e31dd74bef15397555cfea97c859a7232b
tyhu <benken.tyhu@gmail.com>
2016-08-05 00:20:49 -0400

    full dialogue, fake backend

--- app/src/main/java/com/example/asds_template/dm/DialogMovie.java
+++ app/src/main/java/com/example/asds_template/dm/DialogMovie.java
@@ -18,52 +18,140 @@ import java.util.List;
  *
  */
 public class DialogMovie {
-    ArrayList<String> entities;
+
+    //user profile
+    ArrayList<String> user_entities;
+    ArrayList<String> user_movies;
+
+    //current input
+    ArrayList<String> curr_entities;
+    ArrayList<String> curr_types;
+    ArrayList<String> curr_movies;
+
+    //system proposed
+    ArrayList<String> systemMovies;
+
     int rcount;
     public DialogMovie(){
         rcount = 0;
-        entities = new ArrayList<String>();
-        entities.clear();
+        user_entities = new ArrayList<String>();
+        user_entities.clear();
+        user_movies = new ArrayList<String>();
+        user_movies.clear();
+        systemMovies = new ArrayList<String>();
+        systemMovies.clear();
+        curr_entities = new ArrayList<String>();
+        curr_entities.clear();
+        curr_movies = new ArrayList<String>();
+        curr_movies.clear();
+        curr_types = new ArrayList<String>();
+        curr_types.clear();
     }
 
     public HashMap takePolicy(JSONObject nluOut) throws JSONException {
+        String intent = nluOut.getString("label");
         JSONArray inputEntities = nluOut.getJSONArray("entities");
         System.out.println("I am taking action");
+
+        curr_entities.clear();
+        curr_movies.clear();
+        curr_types.clear();
         for(int i=0;i<inputEntities.length();i++){
 
             //movie backend here
-
+            String type = ((JSONArray) inputEntities.get(i)).getString(0);
             String entity =((JSONArray) inputEntities.get(i)).getString(1);
-            if (!entities.contains(entity)){
+            if (!curr_entities.contains(entity)){
                 System.out.println("entity!!! "+entity);
-                entities.add(entity);
+                if (type.equals("movie"))
+                    curr_movies.add(entity);
+                else{
+                    curr_entities.add(entity);
+                    curr_types.add(type);
+                }
+
             }
         }
 
         //==================to nlg=================
         HashMap<String, String> hashMap = new HashMap<String,String>();
         String action = "";
-        String movie = "Mission Impossible";
-        if (entities.size()==0)
-            action = "request_profile";
-        else{
-            action = "recommand_movies";
-            String lastEntity = entities.get(entities.size()-1);
-            if(lastEntity.contains("tom cruise"))
-                movie = "Mission Impossible";
-            else if(lastEntity.contains("melissa mccarthy"))
-                movie = "Ghost Buster";
 
-            hashMap.put("movie",movie);
-            rcount+=1;
+        if(intent.equals("request_recommend"))
+            action = "request_profile";
+        else if(intent.equals("inform_preference")){
+            if (curr_movies.size()==0){
+                //FROM BACKEND: retrieve movies from current entities
+                systemMovies = fakeBackendRetrieveMovie(curr_entities);
+                action = "recommand_movies";
+                hashMap.put("reason_entity",curr_entities.get(0));
+                hashMap.put("reason_type",curr_types.get(0));
+
+            }
+            else{
+                //FROM BACKEND: infer the entity and retrieve movies from inferred entities
+                ArrayList<String> inferredEntities = fakeInferEntities(curr_movies);
+                System.out.println("infer results: "+curr_movies+inferredEntities);
+                systemMovies = fakeBackendRetrieveMovie(inferredEntities);
+                action = "infer+recommand_movie";
+                hashMap.put("source_movies",movieList2Str(curr_movies));
+                hashMap.put("source_num",String.valueOf(curr_movies));
+                hashMap.put("reason_entity", inferredEntities.get(0));
+                hashMap.put("reason_type","director");
+            }
+            hashMap.put("movie_num",String.valueOf(systemMovies.size()));
+            hashMap.put("movie_str",movieList2Str(systemMovies));
+        }
+        else if (intent.equals("decide")){
+            action = "end";
         }
 
-        hashMap.put("action",action);
+
+        hashMap.put("action", action);
         return hashMap;
     }
 
     public void startOver(){
         rcount = 0;
-        entities.clear();
+        user_entities.clear();
+        user_movies.clear();
+        systemMovies.clear();
+    }
+
+    private ArrayList<String> fakeBackendRetrieveMovie(List<String> entities){
+        ArrayList<String> movieList = new ArrayList<String>();
+        if(entities.get(0).equals("sci-fi")){
+            movieList.add("star wars");
+            movieList.add("jurrasic park");
+        }
+        else if (entities.get(0).equals("steven spielberg")){
+            movieList.add("e.t. the extra-terrestrial");
+        }
+        else if (entities.get(0).equals("george lucas")){
+            movieList.add("t-h-x eleven thirty eight");
+        }
+        return movieList;
+    }
+
+    private ArrayList<String> fakeInferEntities(List<String> movies){
+        ArrayList<String> entitesList = new ArrayList<String>();
+        if (movies.contains("jurassic park"))
+            entitesList.add("steven spielberg");
+        else if (movies.contains("star wars"))
+            entitesList.add("george lucas");
+        return entitesList;
+    }
+
+    //concatenate movie strings
+    private String movieList2Str(List<String> movieList){
+        String movieStr = movieList.get(0);
+        if (movieList.size()==1)
+            return movieStr;
+        else{
+            for(int i=1;i<movieList.size()-1;i++)
+                movieStr+=", "+movieList.get(i);
+            movieStr+=" and "+movieList.get(movieList.size()-1);
+            return movieStr;
+        }
     }
 }
--- app/src/main/java/com/example/asds_template/nlg/NLG.java
+++ app/src/main/java/com/example/asds_template/nlg/NLG.java
@@ -22,9 +22,80 @@ public class NLG{
         if(action=="request_profile")
             speakRaw("Ok, tell me what you like");
         else if(action=="recommand_movies"){
-            String movie = hashmap.get("movie");
-            speakRaw("I think you might like "+movie);
+            String moviesStr = hashmap.get("movie_str");
+            String entity = hashmap.get("reason_entity");
+            String type = hashmap.get("reason_type");
+            String movie_num = hashmap.get("movie_num");
+
+            String rawout = "";
+            rawout+=explainStr(entity,type,movie_num);
+            rawout+=" "+moviesStr;
+            speakRaw(rawout);
+
+        }
+        else if (action=="infer+recommand_movie"){
+            String moviesStr = hashmap.get("movie_str");
+            String entity = hashmap.get("reason_entity");
+            String type = hashmap.get("reason_type");
+            String movie_num = hashmap.get("movie_num");
+            String sourceMovieStr = hashmap.get("source_movies");
+            String sourceNum = hashmap.get("source_num");
+
+            String rawout = "";
+            rawout+=inferredExplain(entity,type,sourceNum,sourceMovieStr);
+            rawout+=furtherRecommend(moviesStr,movie_num);
+            speakRaw(rawout);
+        }
+        else if (action=="end"){
+            speakRaw("That's good. enjoy!");
         }
     }
+    private String explainStr(String entity,String type, String movie_num){
+        String out = "";
+        if (type.equals("genre"))
+            if (movie_num.equals("1"))
+                out+="Here is a "+entity+" movie you might like, ";
+            else
+                out+="Here are "+movie_num+" "+entity+" movies you might like, ";
+        else if (type.equals("actor"))
+            if (movie_num.equals("1"))
+                out+="Here is a movie with "+entity+" that you might like, ";
+            else
+                out+="Here are "+movie_num+" movies with "+entity+" that you might like, ";
+        else if (type.equals("director"))
+            if (movie_num.equals("1"))
+                out+="Here is a movie directed by "+entity+", ";
+            else
+                out+="Here are "+movie_num+" movies directed by "+entity+", ";
+        return out;
+    }
 
+    private String inferredExplain(String entity,String type, String movie_num, String movieStr){
+        String out = "";
+        if (type.equals("genre"))
+            if (movie_num.equals("1"))
+                out+=movieStr+" is a good "+entity+" movie, ";
+            else
+                out+=movieStr+" are good "+entity+" movies, ";
+        else if (type.equals("actor"))
+            if (movie_num.equals("1"))
+                out+=movieStr+" is a good movie with "+entity+", ";
+            else
+                out+=movieStr+" are good movies with "+entity+", ";
+        else if (type.equals("director"))
+            if (movie_num.equals("1"))
+                out+=movieStr+" is directed by "+entity+" , ";
+            else
+                out+=movieStr+" are directed by "+entity+" , ";
+        return out;
+    }
+    private String furtherRecommend(String moviesStr,String movie_num){
+        String out = "";
+        if (movie_num.equals("1"))
+            out+="Here is another one, ";
+        else
+            out+="Here are another "+movie_num+", ";
+        out+=moviesStr;
+        return out;
+    }
 }
